88218362055e75cfab5b9d2a901a77dfba3c84fc
Григорий Махалов <mahalof.g@yandex.ru>
2022-07-20 23:11:47 +0700

    PIUPIU

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,16 @@
-from argparse import REMAINDER
-from cgitb import text
-from multiprocessing import context
-from re import template
-from turtle import title
 from django.http import HttpResponse
 from django.shortcuts import render
 
 #Главная страница
 def index(request):    
     template = 'posts/index.html'
-    title = "Yatube"
-    text = "Это главная страница проекта Yatube"
+    title = "YaTube"
+    text = "Это главная страница проекта YaTube"
+    # Словарь
     context = {
-        # В словарь можно передать переменную
         'title' : title,
-        'text': text,
-    }
+        'text': text
+}
     return render(request, template, context) 
 
 def group_posts(request):
@@ -24,6 +19,6 @@ def group_posts(request):
     text = "Здесь будет информация о группах проекта Yatube"
     context = {
         'title' : title,
-        'text': text,
+        'text': text
     }
     return render(request, template, context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+
 # Путь к директории с шаблонами вынесен в переменную:
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
