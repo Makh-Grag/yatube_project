f435f7336ba381b301e96ce29577ce96f5a1b75e
Григорий Махалов <mahalof.g@yandex.ru>
2022-07-23 14:28:02 +0700

    PIAO

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 #Главная страница
-def index(request):    
-    template = 'posts/index.html'
-    title = "YaTube"
-    text = "Это главная страница проекта YaTube"
-    # Словарь
+def index(request):   
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим 
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title' : title,
-        'text': text
-}
-    return render(request, template, context) 
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request):
     template = 'posts/group_list.html'
